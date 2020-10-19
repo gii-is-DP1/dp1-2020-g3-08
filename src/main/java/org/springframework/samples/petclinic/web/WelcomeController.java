1a9fe50e0a3c7dec7dedd4cd2ac3b52dd81f92f4
Marcos <marrodoso@alum.us.es>
2020-10-19 17:44:51 +0200

    Cambios en la pagina de inicio

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,22 +1,47 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.samples.petclinic.model.User;
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
 
 @Controller
 public class WelcomeController {
 	
 	
 	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	    
+	  public String welcome(Map<String, Object> model) {
+		  List<Person> persons= new ArrayList<Person>();
+		  Person person= new Person();
+		  Person person2= new Person();
+		  Person person3= new Person();
+		  Person person4= new Person();
+		  Person person5= new Person();
+		  Person person6= new Person();
+		  person.setFirstName("Marcos");
+		  person.setLastName("Rodriguez");
+		  person2.setFirstName("Gonzalo");
+		  person2.setLastName("Fernandez");
+		  person3.setFirstName("Javier");
+		  person3.setLastName("Grosso");
+		  person4.setFirstName("Juan Luis");
+		  person4.setLastName("Munoz");
+		  person5.setFirstName("Ignacio");
+		  person5.setLastName("Sanabria");
+		  person6.setFirstName("Bogdan");
+		  person6.setLastName("Stefan");
+		  persons.add(person);
+		  persons.add(person2);
+		  persons.add(person3);
+		  persons.add(person4);
+		  persons.add(person5);
+		  persons.add(person6);
+		  model.put("persons", persons);
+		  model.put("title", "Prueba");
+		  model.put("group", "G3-08");
 
 	    return "welcome";
 	  }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,15 +1,30 @@
-<%@ page session="false" trimDirectiveWhitespaces="true" %>
-<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<%@ taglib prefix="petclinic" tagdir="/WEB-INF/tags" %>
-<!-- %@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %-->  
+	
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false" trimDirectiveWhitespaces="true"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="petclinic" tagdir="/WEB-INF/tags"%>
+<!-- %@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %-->
 
 <petclinic:layout pageName="home">
-    <h2><fmt:message key="welcome"/></h2>
-    <div class="row">
-        <div class="col-md-12">
-            <spring:url value="/resources/images/us-logo.png" htmlEscape="true" var="usLogo"/>
-            <img class="img-responsive" src="${usLogo}"/>
-        </div>
-    </div>
+	<h2>
+		<fmt:message key="welcome" />
+	</h2>
+	<div class="row">
+		<h2>Project ${title}</h2>
+		<p>
+		<h2>Grupo ${group}</h2>
+		</p>
+		<p>
+			<u1> <c:forEach items="${persons}" var="person">
+				<li>${person.firstName}${person.lastName}</li>
+			</c:forEach> </u1>
+		</p>
+		</div>
+		<div class="col-md-12">
+			<spring:url value="/resources/images/us-logo.png" htmlEscape="true"
+				var="usLogo" />
+			<img class="img-responsive" src="${usLogo}" />
+		</div>
+	</div>
 </petclinic:layout>
