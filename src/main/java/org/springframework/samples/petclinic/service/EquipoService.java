8b835877e1af9fa472272bbe04547213bb476d84
gonferjim <gonferjim@alum.us.es>
2020-11-13 10:48:21 +0100

    Fix errores merge Develop <- gonferjim

--- src/main/java/org/springframework/samples/petclinic/service/EquipoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/EquipoService.java
@@ -18,24 +18,11 @@ package org.springframework.samples.petclinic.service;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Equipo;
-import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.model.UsuarioRegistrado;
-import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.model.Visit;
 import org.springframework.samples.petclinic.repository.EquipoRepository;
-import org.springframework.samples.petclinic.repository.OwnerRepository;
-import org.springframework.samples.petclinic.repository.PetRepository;
-import org.springframework.samples.petclinic.repository.VetRepository;
-import org.springframework.samples.petclinic.repository.VisitRepository;
-import org.springframework.samples.petclinic.service.exceptions.DuplicatedPetNameException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
 
 @Service
@@ -47,13 +34,13 @@ public class EquipoService {
 	@Autowired
 	public EquipoService(EquipoRepository equipoRepository) {
 		this.equipoRepository = equipoRepository;
-	}		
+	}
 
-	@Transactional(readOnly = true)	
+	@Transactional(readOnly = true)
 	public Collection<Equipo> findEquipos() throws DataAccessException {
 		return equipoRepository.findAll();
-	}	
-	
+	}
+
 	@Transactional
 	public void saveEquipo(Equipo equipo) throws DataAccessException {
 		equipoRepository.save(equipo);
