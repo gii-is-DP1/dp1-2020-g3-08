81613187a9fd6413852260db9fe7a16969e7eaab
gonferjim <gonferjim@alum.us.es>
2020-11-15 13:01:31 +0100

    Realizado tareas de Noticia con Javier
    Creacion de Entidad, Publicar noticia y Visualización de noticia

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -35,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(final HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/resources/**", "/webjars/**", "/h2-console/**").permitAll()
 		.antMatchers(HttpMethod.GET, "/", "/oups").permitAll()
+		.antMatchers("/noticias/**").permitAll()
 		.antMatchers("/users/new").permitAll()
 		.antMatchers("/users/**").hasAnyAuthority("user","admin")
 		.antMatchers("/admin/**").hasAnyAuthority("admin")
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO genres VALUES (1, 'hombre');
 INSERT INTO genres VALUES (2, 'mujer');
+
+INSERT INTO noticias VALUES (1, '2010-09-07','titulo', 'texto');
